b6d04817a192c88dae60fadc9b94f1edc200af30
bravekorean <wnsqja4646@gmail.com>
2026-06-17 15:02:28 +0900

    first commit

---
+++ src/test/java/com/example/sideworks/SideworksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sideworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SideworksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
