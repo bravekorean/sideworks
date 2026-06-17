af5053304b7113a6ff2bb9be36ad094c81ec3c92
bravekorean <wnsqja4646@gmail.com>
2026-06-17 14:56:05 +0900

    first commit

---
+++ src/main/java/com/example/sideworks/SideworksApplication.java
@@ -0,0 +1,13 @@
+package com.example.sideworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SideworksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SideworksApplication.class, args);
+    }
+
+}
